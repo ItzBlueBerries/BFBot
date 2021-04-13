59057d398b77082ec91b5166000e6b70d1286f81
FruitBerries <80174970+ItzBlueBerries@users.noreply.github.com>
2021-04-12 20:53:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,5 +10,9 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('message', message => {
+	console.log(message.content);
+});
+
 // login to Discord with your app's token
 client.login('your-token-goes-here');
