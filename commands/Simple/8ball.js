e63c5d67eb70fb516c5e39b13827941f27354b79
Jack5 <jack5answers@gmail.com>
2021-04-13 17:25:35 +1000

    8ball + neave

---
+++ commands/Simple/8ball.js
@@ -0,0 +1,35 @@
+const discord = require("discord.js");
+
+module.exports = {
+	name: "8ball",
+	category: "Simple",
+	description: "Get your fortune told from the Magic 8-Ball.",
+	usage: "8ball",
+	run: async (client, message, args) => {
+		let responses = [
+			'It is certain.',
+			'It is decidedly so.',
+			'Without a doubt.',
+			'Yes - definitely.',
+			'You may rely on it.',
+			'As I see it, yes.',
+			'Most likely.',
+			'Outlook good.',
+			'Yes.',
+			'Signs point to yes.',
+			'Reply hazy, try again.',
+			'Ask again later.',
+			'Better not tell you now.',
+			'Cannot predict now.',
+			'Concentrate and ask again.',
+			"Don't count on it.",
+			'My reply is no.',
+			'My sources say no.',
+			'Outlook not so good.',
+			'Very doubtful.'
+		];
+		let embed = new discord.MessageEmbed()
+			.setTitle('🎱 ' + responses[Math.floor(Math.random() * 20)]);
+		message.reply(embed);
+	}
+}
