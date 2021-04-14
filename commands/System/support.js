9a25a986b8dbfb6baf9ef7c8a3dae6e9b15565d6
Jack5 <jack5answers@gmail.com>
2021-04-14 22:11:47 +1000

    Supprt update

--- commands/System/support.js
+++ commands/System/support.js
@@ -11,7 +11,7 @@ module.exports = {
 		let embed = new discord.MessageEmbed()
 			.setColor(global.embedColor)
 			.setTitle(`Support Center`)
-			.setDescription(`*He beeps, he bleeps, but most of all, he's sweet.*\n\nBFBot is being developed by Fruitsy and Jack5 to be a general purpose Discord bot for recreational use.`)
+			.setDescription(`*He beeps, he bleeps, but most of all, he's sweet.*\n\nFuncFriend is being developed by Fruitsy and Jack5 to be a general purpose Discord bot for recreational use.`)
 			.addFields(
 				{name: `Fruitsy - Lead programmer`, value: '`Fruitsy#6513\`'},
 				{name: `Jack5 - Programmer and hosting`, value: '`Jack5#1720` https://discord.gg/scRdSXY'}
